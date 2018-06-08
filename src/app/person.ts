884004c104b34f075375efed216579ed825ff309
Aqs Malhotra <aqsm2011@gmail.com>
2018-06-08 09:16:03 -0400

    add Person Model - Part 1

---
+++ src/app/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+
+  //add method getAge
+  /*getAge() {
+    today: Date = new Date();
+    return today.getFullYear() - dateOfBirth.getFullYear();
+  }*/
+}
