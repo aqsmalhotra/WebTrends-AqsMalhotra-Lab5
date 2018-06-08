d7bc442a69a0dde3e488131b0b070629684d8d78
Aqs Malhotra <aqsm2011@gmail.com>
2018-06-08 09:25:18 -0400

    add guestlist component - part 3

---
+++ src/app/guestlist/guestlist.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from '../people.service';
+import { Person } from '../person';
+import { query } from '@angular/core/src/render3/query';
+
+@Component({
+  selector: 'app-guestlist',
+  templateUrl: './guestlist.component.html',
+  styleUrls: ['./guestlist.component.css']
+})
+export class GuestlistComponent implements OnInit {
+
+  searchGuest: Person = new Person();
+
+  guests: Person[] = [];
+
+  findPeople(queryPerson?: Person) {
+
+    this.peopleService.getPeople().subscribe(data => this.guests = data);
+
+    if(queryPerson !== undefined) {
+      this.peopleService.getPeople(queryPerson).subscribe( data => this.guests = data);
+    }
+  }
+
+  constructor(private peopleService: PeopleService) { }
+
+  ngOnInit() {
+    this.findPeople();
+  }
+
+}
--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -8,17 +8,32 @@ import { Observable, of } from 'rxjs';
 export class PeopleService {
 
   people: Person[] = [ //* create guest array
-      {id: 1, firstName: 'Bill', lastName: 'Wright', dateOfBirth: new Date('01/01/1950')},
-      {id: 2, firstName: 'Bernie', lastName: 'Monette', dateOfBirth: new Date('01/01/1955')},
-      {id: 3, firstName: 'Sean', lastName: 'Doyle', dateOfBirth: new Date('01/01/1970')},
+     {id: 1, firstName: 'Lee', lastName: 'Dinh', dateOfBirth: new Date('02/09/1992')},
+     {id: 2, firstName: 'Bill', lastName: 'Wright', dateOfBirth: new Date('01/01/1950')},
+     {id: 3, firstName: 'Bernie', lastName: 'Monette', dateOfBirth: new Date('01/01/1955')},
+     {id: 4, firstName: 'Sean', lastName: 'Doyle', dateOfBirth: new Date('01/01/1970')},
 
-      ];
+     ];
 
-    //* create func to return guest array
-    getPeople(): Observable< Person[] > {
+   //* create func to return guest array
+   getPeople(peopleOfInterest?: Person): Observable< Person[] > {
 
-      return of(this.people);
-    }
-    constructor() { }
+     if(peopleOfInterest !== undefined) {
+
+       let peopleResults: Person[] = []; //create empty array to obtain result
+
+       for(let p of this.people) { //* loop through people arr
+         if(peopleOfInterest.firstName.toLowerCase() === p.firstName.toLowerCase()) {
+             peopleResults.push(p);
+         }else if(peopleOfInterest.lastName.toLowerCase() === p.lastName.toLowerCase()) {
+             peopleResults.push(p);
+         }
+
+       }
+       return of(peopleResults);
+     }
+     return of(this.people);
+   }
+   constructor() { }
 
 }
