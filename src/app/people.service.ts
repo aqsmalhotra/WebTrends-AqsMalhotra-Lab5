e47748b7643b45517f2ad71d99149e76ca165b6e
Aqs Malhotra <aqsm2011@gmail.com>
2018-06-08 09:19:36 -0400

    add people service - part 2

---
+++ src/app/people.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Person } from './person';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+
+  people: Person[] = [ //* create guest array
+      {id: 1, firstName: 'Bill', lastName: 'Wright', dateOfBirth: new Date('01/01/1950')},
+      {id: 2, firstName: 'Bernie', lastName: 'Monette', dateOfBirth: new Date('01/01/1955')},
+      {id: 3, firstName: 'Sean', lastName: 'Doyle', dateOfBirth: new Date('01/01/1970')},
+
+      ];
+
+    //* create func to return guest array
+    getPeople(): Observable< Person[] > {
+
+      return of(this.people);
+    }
+    constructor() { }
+
+}
